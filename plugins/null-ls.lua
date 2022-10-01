6310b187b9808d91201dfea106f7011bc7049f2e
Nikolai Perlt <nikolaiperlt@outlook.com>
2022-10-01 22:35:45 +0000

    stuff for migration

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -1,11 +1,7 @@
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local null_ls_config = function(config) -- overrides `require("null-ls").setup(config)`
-  -- config variable is the default configuration table for the setup functino call
   local null_ls = require "null-ls"
   local formatting = null_ls.builtins.formatting
-  -- Check supported formatters and linters
-  -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-  -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
   config.sources = {
     -- Set a formatter
     formatting.black.with { extra_args = { "--line-length=79" } },
@@ -20,20 +16,6 @@ local null_ls_config = function(config) -- overrides `require("null-ls").setup(c
     -- C#
     null_ls.builtins.formatting.csharpier,
   }
-  -- set up null-ls's on_attach function
-  -- NOTE: You can remove this on attach function to disable format on save
-   config.on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = bufnr })
-                end,
-            })
-        end
-      end
   return config -- return final config table to use in require("null-ls").setup(config)
 end
 
